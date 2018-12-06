3df764ae082352b4b1f92c036d73b39115937a59
shennychacko <shennychacko80@gmail.com>
2018-12-05 20:10:27 -0700

    initial commit

---
+++ src/main/java/com/example/microservices/zuulgatewayserver/ZuulGatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservices.zuulgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayServerApplication.class, args);
+	}
+}
